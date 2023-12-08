c9202a668a5edebc276b4c974f694b0f6f070a1b
wg138940 <wg138940@outlook.com>
2023-12-08 23:19:43 +0800

    + windows icon

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+extern crate embed_resource;
+
 fn main() {
+    if cfg!(target_os = "windows") {
+        embed_resource::compile("resource/windows/resources.rc", embed_resource::NONE);
+    }
+
     slint_build::compile("ui/app-window.slint").unwrap();
 }
