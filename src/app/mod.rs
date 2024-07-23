04a3b0940c1be83b75bd3dc22cd2cae0a2ecfd54
wg138940 <wg138940@outlook.com>
2024-07-23 11:11:06 +0800

    * join workers in app::run

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -46,7 +46,10 @@ use asset_worker::AssetWorker;
 #[allow(unused)]
 use auth_worker::AuthWorker;
 use std::sync::{Arc, RwLock};
-use tokio::sync::{mpsc, oneshot};
+use tokio::{
+    sync::{mpsc, oneshot},
+    task::JoinHandle,
+};
 use tokio_util::sync::CancellationToken;
 use ui::*;
 use utils::user_state::UserStateDBStore;
@@ -60,7 +63,7 @@ pub async fn run() -> Result<(), slint::PlatformError> {
     let mut user_state = UserStateDBStore::new();
     _ = user_state
         .load_from_db()
-        .map_err(|e| println!("[main] Error loading user state from DB {e}"));
+        .map_err(|e| println!("[app::run] Error loading user state from DB {e}"));
     let user_state_data_or_null = user_state.user_state_data();
     let auth_client = create_auth_client(Arc::new(RwLock::new(user_state)));
 
@@ -69,7 +72,7 @@ pub async fn run() -> Result<(), slint::PlatformError> {
 
     let mut api_worker = ApiWorker::new(auth_client);
     let (tx_api_command, rx_api_command) = mpsc::channel(32);
-    tokio::spawn({
+    let api_worker_join_handle = tokio::spawn({
         let stop = stop.clone();
         async move {
             api_worker.run(rx_api_command, stop).await;
@@ -85,7 +88,7 @@ pub async fn run() -> Result<(), slint::PlatformError> {
 
     let (tx_auth_command, rx_auth_command) = mpsc::channel(16);
     let stop = stop.clone();
-    tokio::spawn({
+    let auth_worker_join_handle = tokio::spawn({
         let stop = stop.clone();
         async move {
             auth_worker.run(rx_auth_command, stop).await;
@@ -95,7 +98,7 @@ pub async fn run() -> Result<(), slint::PlatformError> {
     let asset_client = create_asset_client();
     let mut asset_worker = AssetWorker::new(asset_client);
     let (tx_asset_command, rx_asset_command) = mpsc::channel(32);
-    tokio::spawn({
+    let asset_worker_join_handle = tokio::spawn({
         let stop = stop.clone();
         async move {
             asset_worker.run(rx_asset_command, stop).await;
@@ -116,7 +119,7 @@ pub async fn run() -> Result<(), slint::PlatformError> {
     #[cfg(target_os = "windows")]
     let default_webview_installation_found: bool = {
         let ver = utils::webview2::test_installation_ver();
-        println!("[WebView2] installation found: {ver:?}");
+        println!("[app::run] WebView2 installation found: {ver:?}");
         ver.is_some()
     };
     #[cfg(not(target_os = "windows"))]
@@ -151,11 +154,11 @@ pub async fn run() -> Result<(), slint::PlatformError> {
             Ok(Ok(())) => true,
             Ok(Err(AuthError::LaunchWebView)) | Ok(Err(AuthError::ProcessExited(_))) => false,
             Ok(Err(e)) => {
-                println!("Unknown launch error checking webview availability: {e}");
+                println!("[app::run] Unknown launch error checking webview availability: {e}");
                 false
             }
             Err(e) => {
-                println!("Unknown error checking webview availability: {e}");
+                println!("[app::run] Unknown error checking webview availability: {e}");
                 false
             }
         };
@@ -177,22 +180,41 @@ pub async fn run() -> Result<(), slint::PlatformError> {
             tokio::spawn(async move { ui_context.session_controller.auth().await });
         }
     }
-    ui.run()
+    ui.show()?;
+    slint::run_event_loop()?;
+
+    // join workers with timeout
+    stop.cancel();
+    tokio::select! {
+        _ = async move {
+            tokio::join!(
+                join_worker("api_worker", api_worker_join_handle),
+                join_worker("auth_worker", auth_worker_join_handle),
+                join_worker("asset_worker", asset_worker_join_handle))
+        } => {},
+        _ = tokio::time::sleep(consts::WORKER_JOIN_TIMEOUT) => {
+            utils::notification::toast("可露希尔客户端非正常退出", None, "退出花的时间过长，已强制停止。如果频繁遇到请反馈问题。", None);
+            let err_info = format!("[app::run] Timed out joining workers! ({:?})", consts::WORKER_JOIN_TIMEOUT);
+            println!("{err_info}");
+            return Err(slint::PlatformError::Other(err_info));
+        }
+    };
+    Ok(())
 }
 
 fn create_auth_client(user_state: Arc<RwLock<dyn UserState>>) -> AuthClient {
     use reqwest_retry::policies::ExponentialBackoff;
     use reqwest_retry::RetryTransientMiddleware;
-    use std::time::Duration;
     let client = AuthClient::default_client_builder()
         .use_rustls_tls()
         .gzip(true)
         .brotli(true)
-        .connect_timeout(Duration::from_secs(5))
+        .connect_timeout(consts::AUTH_CLIENT_CONNECT_TIMEOUT)
         .build()
         .unwrap();
 
-    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
+    let retry_policy: ExponentialBackoff =
+        ExponentialBackoff::builder().build_with_max_retries(consts::AUTH_CLIENT_MAX_RETRIES);
     let client_with_middlewares = reqwest_middleware::ClientBuilder::new(client)
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
         .build();
@@ -209,7 +231,6 @@ fn create_asset_client() -> AssetClient {
     use http_cache_reqwest::Cache;
     use reqwest_retry::policies::ExponentialBackoff;
     use reqwest_retry::RetryTransientMiddleware;
-    use std::time::Duration;
     use utils::cache_control::default_cache_mode_fn;
     use utils::cache_manager::DBCacheManager;
 
@@ -217,11 +238,12 @@ fn create_asset_client() -> AssetClient {
         .use_rustls_tls()
         .gzip(true)
         .brotli(true)
-        .connect_timeout(Duration::from_secs(5))
+        .connect_timeout(consts::ASSET_CLIENT_CONNECT_TIMEOUT)
         .build()
         .unwrap();
 
-    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(2);
+    let retry_policy =
+        ExponentialBackoff::builder().build_with_max_retries(consts::ASSET_CLIENT_MAX_RETRIES);
     let client_with_middlewares = reqwest_middleware::ClientBuilder::new(client)
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
         .with(Cache(HttpCache {
@@ -238,4 +260,25 @@ fn create_asset_client() -> AssetClient {
         env::override_asset_server().unwrap_or(arkhost_api::consts::asset::API_BASE_URL),
         client_with_middlewares,
     )
+}
+
+async fn join_worker(worker_name: &str, join_handle: JoinHandle<()>) {
+    match join_handle.await {
+        Ok(_) => {
+            println!("[app::run] Joined worker '{worker_name}'");
+        }
+        Err(e) => {
+            println!("[app::run] Error joining worker '{worker_name}': {e}");
+        }
+    }
+}
+
+mod consts {
+    use std::time::Duration;
+
+    pub const AUTH_CLIENT_CONNECT_TIMEOUT: Duration = Duration::from_secs(5);
+    pub const ASSET_CLIENT_CONNECT_TIMEOUT: Duration = Duration::from_secs(5);
+    pub const AUTH_CLIENT_MAX_RETRIES: u32 = 3;
+    pub const ASSET_CLIENT_MAX_RETRIES: u32 = 2;
+    pub const WORKER_JOIN_TIMEOUT: Duration = Duration::from_secs(20);
 }
